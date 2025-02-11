ae804bb862227b33b0892e9a0787d4e74f7b8d6b
weeday01 <arthurfillietta@gmail.com>
2025-02-11 18:23:21 -0300

    simplificação no carregamento dos dados

--- analise_diamante.py
+++ analise_diamante.py
@@ -1,14 +1,13 @@
-import os
 import pandas as pd
 
-path = os.getcwd() #os.listdir()
-
-arquivo = 'c:\\Users\\arthu\\OneDrive\\Documentos\\analise_diamantes\\archive.zip'
-
-dados = pd.read_csv(arquivo, index_col = 0, sep=',', decimal='.')
 
 
-#alteração no codigo 
+arquivo = 'diamonds.csv'
+
+data = pd.read_csv(arquivo, index_col = 0, sep=',', decimal='.')
+
+
+
 
 
 
